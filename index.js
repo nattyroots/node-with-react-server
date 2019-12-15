1d71bb21e9cd22972f2d628fd3ee8474a88d18c0
nattyroots <nattyroots96@gmail.com>
2019-12-15 18:14:04 +0200

    add mongoose connection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
+const keys = require("./config/keys/keys");
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
 require("./routs/authRoutes")(app);
 
