049997e5c5603bda7322a6e50c20156b3b31bf4d
nattyroots <nattyroots96@gmail.com>
2019-12-18 17:14:41 +0200

    end of lecture 82

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,10 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import App from "./components/App";
 import reducers from "./reducers";
-const store = createStore(reducers, {}, applyMiddleware());
+import reduxThunk from "redux-thunk";
+
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(
   <Provider store={store}>
     <App />
