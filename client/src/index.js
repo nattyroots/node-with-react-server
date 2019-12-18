fbfc10c5c6f7f3a08158abe65a3ad77909bff4bb
nattyroots <nattyroots96@gmail.com>
2019-12-18 14:06:56 +0200

    add basic redux/react

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import App from "./components/App";
+
+const store = createStore(() => [], {}, applyMiddleware());
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
