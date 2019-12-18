572c29be6e98886a77c753f29062e75d4c9824e6
nattyroots <nattyroots96@gmail.com>
2019-12-18 14:28:55 +0200

    add auth Reducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import App from "./components/App";
-
-const store = createStore(() => [], {}, applyMiddleware());
+import reducers from "./reducers";
+const store = createStore(reducers, {}, applyMiddleware());
 ReactDOM.render(
   <Provider store={store}>
     <App />
